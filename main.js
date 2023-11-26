3a10a084c1293adbd6fe3bc1ebfbfed0fdecd3a4
Eduardo Antonio Martinez <eduardoa4848@Outlook.es>
2023-11-26 13:16:34 -0600

    Se crea el software de escritorio y la conexion

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  mainWindow.loadURL('https://992mpm64-4000.usw3.devtunnels.ms/');
+  //mainWindow.loadFile('index.html'); // Reemplaza 'index.html' con el nombre de tu archivo HTML principal.
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
