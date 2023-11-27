abc00461c0b1d544738ccf56465cfdbc658ec948
Eduardo Antonio Martinez <eduardoa4848@Outlook.es>
2023-11-26 18:56:17 -0600

    Se agrega logo y quita las opciones de electron

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow , Menu} = require('electron');
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1248,
+    height: 731,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    icon: 'FUD.png'  // Ruta al archivo de icono
   });
   mainWindow.loadURL('https://992mpm64-4000.usw3.devtunnels.ms/');
   //mainWindow.loadFile('index.html'); // Reemplaza 'index.html' con el nombre de tu archivo HTML principal.
 
+  //
+  Menu.setApplicationMenu(null);
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
